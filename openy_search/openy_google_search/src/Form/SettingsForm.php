05065aa49e635c8b238f4195ef2ba46c48d79365
Avi Schwab <froboy@gmail.com>
2023-12-13 17:00:27 -0600

    DS-1254 feat: Add config options for Google Custom Search and update docs

--- openy_search/openy_google_search/src/Form/SettingsForm.php
+++ openy_search/openy_google_search/src/Form/SettingsForm.php
@@ -68,12 +68,47 @@ class SettingsForm extends ConfigFormBase {
     $form['google_engine_id'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Google Search Engine ID'),
+      '#description' => $this->t('Go to the <a href="https://programmablesearchengine.google.com/controlpanel/all" target="_blank">Google Control Panel</a> then copy the ID from the search engine overview.'),
       '#size' => 40,
       '#default_value' => !empty($config->get('google_engine_id')) ? $config->get('google_engine_id') : '',
       '#required' => TRUE,
     ];
 
+    $form['search_page_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search page ID'),
+      '#description' => $this->t('Find the node id of the search results page by editing the page and looking in the URL for <code>/node/{node_id}/edit</code>'),
+      '#size' => 40,
+      '#default_value' => !empty($config->get('search_page_id')) ? $config->get('search_page_id') : '',
+      '#required' => TRUE,
+    ];
+
+    $form['search_query_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search query key'),
+      '#description' => $this->t('The argument preceding the search string in the URL. For example, in <code>/search?q=swim</code>, the query key is <code>q</code>. Google does not allow configuring this value.'),
+      '#size' => 40,
+      '#default_value' => !empty($config->get('search_query_key')) ? $config->get('search_query_key') : '',
+      '#required' => TRUE,
+      '#disabled' => 'disabled',
+    ];
+
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state)
+  {
+    $search_page_id = $form_state->getValue('search_page_id');
+
+    // Set an error if the page id is not a number.
+    if (!is_numeric($search_page_id)) {
+      $form_state
+        ->setErrorByName('search_page_id', $this
+        ->t('The Search page ID must be a number.'));
+    }
   }
 
   /**
@@ -83,6 +118,8 @@ class SettingsForm extends ConfigFormBase {
     $values = $form_state->getValues();
     $config = $this->config('openy_google_search.settings');
     $config->set('google_engine_id', $values['google_engine_id']);
+    $config->set('search_page_id', $values['search_page_id']);
+    $config->set('search_query_key', $values['search_query_key']);
     $config->save();
 
     $this->cacheTagsInvalidator->invalidateTags($config->getCacheTags());
